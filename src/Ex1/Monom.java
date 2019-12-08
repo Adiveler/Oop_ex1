ef0494c892cb37fc79fbbb94bd7f7c941efff74c
Adiveler <58662472+Adiveler@users.noreply.github.com>
2019-12-08 18:30:54 +0200

    First upload

---
+++ src/Ex1/Monom.java
@@ -0,0 +1,143 @@
+
+package Ex1;
+
+import java.util.Comparator;
+
+/**
+ * This class represents a simple "Monom" of shape a*x^b, where a is a real number and a is an integer (summed a none negative), 
+ * see: https://en.wikipedia.org/wiki/Monomial 
+ * The class implements function and support simple operations as: construction, value at x, derivative, add and multiply. 
+ * @author Boaz
+ *
+ */
+public class Monom implements function{
+	public static final Monom ZERO = new Monom(0,0);
+	public static final Monom MINUS1 = new Monom(-1,0);
+	public static final double EPSILON = 0.0000001;
+	public static final Comparator<Monom> _Comp = new Monom_Comperator();
+	public static Comparator<Monom> getComp() {return _Comp;}
+	public Monom(double a, int b){
+		this.set_coefficient(a);
+		this.set_power(b);
+	}
+	public Monom(Monom ot) {
+		this(ot.get_coefficient(), ot.get_power());
+	}
+	
+	public double get_coefficient() {
+		return this._coefficient;
+	}
+	public int get_power() {
+		return this._power;
+	}
+	/** 
+	 * this method returns the derivative monom of this.
+	 * @return
+	 */
+	public Monom derivative() {
+		if(this.get_power()==0) {return getNewZeroMonom();}
+		return new Monom(this.get_coefficient()*this.get_power(), this.get_power()-1);
+	}
+	@Override
+	public double f(double x) {
+		double ans=0;
+		double p = this.get_power();
+		ans = this.get_coefficient()*Math.pow(x, p);
+		return ans;
+	} 
+	public boolean isZero() {return this.get_coefficient() == 0;}
+	// ***************** add your code below **********************
+	public Monom(String s) {
+		s = s.replaceAll("\\s+","");
+		boolean ispositive = true;
+		if (s.charAt(0)=='-') {
+			ispositive = false;
+			s = s.substring(1);
+		}
+		if (s.charAt(0)=='+') {
+			s = s.substring(1);
+		}
+		if (s.charAt(0)=='x')
+			s = "1" + s;
+		
+		if (!s.contains("^")) {
+			if (!s.contains("x"))
+				s = s+"x^0";
+			else
+				s = s+"^1";
+		}
+		String[] mcoefficient = s.split("x\\^");
+		double coef = Double.parseDouble(mcoefficient[0]);
+		if (!ispositive)
+			coef = coef*(-1);
+		this.set_coefficient(coef);
+		this.set_power(Integer.parseInt(mcoefficient[1]));
+		
+	}
+	
+	public void add(Monom m) {
+		if (m.get_power() == _power) {
+			_coefficient = m.get_coefficient() + _coefficient;
+		}
+	}
+	
+	public void multipy(Monom d) {
+		_power = d.get_power() + _power;
+		_coefficient = d.get_coefficient() * _coefficient;
+	}
+	
+	public String toString() {
+		String ans = "";
+		if (this._coefficient > 0)
+			ans = "+";
+		ans = ans+this._coefficient+"x^"+this.get_power();
+		return ans;
+	}
+	// you may (always) add other methods.
+	
+	public boolean equals(Monom m) {	
+		if (comp(_coefficient, m.get_coefficient()) && _power == m.get_power())
+			return true;
+		return false;
+	}
+	public static boolean comp (double a, double b ) {
+		return comp(a,b,EPSILON);
+	}
+	public static boolean comp (double a, double b, double eps ) {
+		double sub = a - b;
+		if (sub <= eps && sub >= -eps)
+			return true;
+		return false;
+	}
+	
+	public static int comperator(Monom a, Monom b) {
+		return (a._power - b._power);
+	}
+	//****************** Private Methods and Data *****************
+	
+
+	private void set_coefficient(double a){
+		this._coefficient = a;
+	}
+	
+	private void set_power(int p) {
+		if(p<0) {throw new RuntimeException("ERR the power of Monom should not be negative, got: "+p);}
+		this._power = p;
+	}
+	private static Monom getNewZeroMonom() {return new Monom(ZERO);}
+	private double _coefficient; 
+	private int _power;
+	
+	
+	
+	@Override
+	public function initFromString(String s) {
+		return new Monom(s);
+	}
+	@Override
+	public function copy() {
+		return new Monom(this);
+	}
+	
+	
+}
---
+++ src/Ex1/Polynom.java
@@ -0,0 +1,292 @@
+package Ex1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.function.Predicate;
+
+
+/**
+ * This class represents a Polynom with add, multiply functionality, it also should support the following:
+ * 1. Riemann's Integral: https://en.wikipedia.org/wiki/Riemann_integral
+ * 2. Finding a numerical value between two values (currently support root only f(x)=0).
+ * 3. Derivative
+ * 
+ * @author Boaz
+ *
+ */
+public class Polynom implements Polynom_able{
+
+	private ArrayList<Monom> monoms = new ArrayList<Monom>();
+	/**
+	 * Zero (empty polynom)
+	 */
+	public Polynom() {
+		;
+	}
+	/**
+	 * init a Polynom from a String such as:
+	 *  {"x", "3+1.4X^3-34x", "(2x^2-4)*(-1.2x-7.1)", "(3-3.4x+1)*((3.1x-1.2)-(3X^2-3.1))"};
+	 * @param s: is a string represents a Polynom
+	 */
+	public Polynom(String s) {
+		s = s.replaceAll("\\s+","");
+		s = s.toLowerCase(); // Change 'X' to 'x'
+		Polynom p = inBracks(s);
+		//System.out.println("in constructor: "+p);
+		add(p);
+	}
+	//this function focus inside the brackets
+	/**
+	 * get String without "()" characters
+	 * @return Polynom
+	 */
+	private Polynom inBracks (String s) {
+		s = s + "+0";
+		Polynom p = new Polynom();
+		int init = 0;
+		for (int i = 1; i < s.length(); i++) {
+			if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+				String ms = s.substring(init, i);
+				Monom m = new Monom (ms);
+				init = i;
+				p.add(m);
+			}
+		}
+		return p;
+	}
+	/**
+	 * replace x with a number and calculate the polynom
+	 * @return number
+	 */
+	@Override
+	public double f(double x) {
+		double ans = 0;
+		for (int i = 0; i < monoms.size(); i++) {
+			ans = ans + monoms.get(i).f(x);
+		}
+		return ans;
+	}
+
+	
+	/**
+	 * Add p1 to this Polynom
+	 * @param p1
+	 */
+	@Override
+	public void add(Polynom_able p1) {
+		Iterator<Monom> iter = p1.iteretor();
+		while (iter.hasNext())
+			add(iter.next());
+	}
+
+	/**
+	 * Add m1 to this Polynom
+	 * @param m1 Monom
+	 */
+	@Override
+	public void add(Monom m1) {
+		for (int i = 0; i < monoms.size(); i++) {
+			if (monoms.get(i).get_power() == m1.get_power()) {//if we have 2 monoms with the same power (like 3x^4 + 6x^4)
+				int power = m1.get_power();
+				double coefficient = m1.get_coefficient() + monoms.get(i).get_coefficient();
+				monoms.remove(i);
+				Monom m2 = new Monom (coefficient, power);
+				monoms.add(m2);
+				Collections.sort(monoms, new Sortbypower());
+				return;
+			}
+		}
+		Monom copy = new Monom (m1);
+		monoms.add(copy);
+		
+
+		Collections.sort(monoms, new Sortbypower());
+
+	}
+	/**
+	 * Subtract p1 from this Polynom
+	 * @param p1
+	 */
+	@Override
+	public void substract(Polynom_able p2) {
+		Polynom_able p1 = new Polynom (p2.toString());
+		Iterator<Monom> iter = p1.iteretor();
+		while (iter.hasNext()) {
+			Monom m = new Monom (iter.next());
+			m.multipy(Monom.MINUS1);
+			add(m);
+		}
+	}
+	/**
+	 * Multiply this Polynom by p1
+	 * @param p1
+	 */
+	@Override
+	public void multiply(Polynom_able p1) {
+		Iterator<Monom> iter = p1.iteretor();
+		ArrayList<Polynom_able> poly = new ArrayList<Polynom_able>();
+		while (iter.hasNext()) {
+			Polynom_able tmp = this.copy();
+			Monom m = new Monom (iter.next());
+			tmp.multiply(m);
+			poly.add(tmp);			
+		}
+		monoms.clear();
+		for (Polynom_able polynom_able_ : poly) {
+			add(polynom_able_);
+		}
+	}
+	/**
+	 * Test if this Polynom is logically equals to p1.
+	 * @param p1
+	 * @return true if this polynom represents the same function as p1
+	 */
+
+	public boolean equals(Polynom_able p1) {
+				 		
+		boolean eq = true;
+		Iterator<Monom> iter_1 = p1.iteretor();
+		Iterator<Monom> iter_this = this.iteretor();
+		
+		while (iter_1.hasNext() && iter_this.hasNext()) {
+			Monom m1 = iter_1.next();
+			Monom mThis =iter_this.next();
+			
+			if(!m1.equals(mThis))
+				eq = false;
+			
+		}
+		if(iter_1.hasNext() || iter_this.hasNext())
+			eq = false;
+		return eq;
+	}
+	/**
+	 * Test if this is the Zero Polynom
+	 * @return
+	 */
+	@Override
+	public boolean isZero() {
+		boolean zero = true;
+		for (int i = 0; i < monoms.size(); i++) {
+			if (monoms.get(i).get_coefficient() != 0)
+				zero = false;
+		}
+		return zero;
+	}
+	/**
+	 * Compute a value x' (x0<=x'<=x1) for with |f(x')| < eps
+	 * assuming (f(x0)*f(x1)<=0, else should throws runtimeException 
+	 * computes f(x') such that:
+	 * 	(i) x0<=x'<=x1 && 
+	 * 	(ii) |f(x')|<eps
+	 * @param x0 starting point
+	 * @param x1 end point
+	 * @param eps>0 (positive) representing the epsilon range the solution should be within.
+	 * @return an approximated value (root) for this (cont.) function 
+	 */
+	@Override
+	public double root(double x0, double x1, double eps) {
+		if (f(x0)*f(x1) > 0)
+			throw new RuntimeException();
+		double x = (x0 + x1)/2;
+		if (Monom.comp(f(x), 0, eps))
+			return x;
+		if (f(x0)*f(x) > 0)
+			return root(x,x1,eps);
+		else
+			return root(x0,x,eps);
+	}
+	/**
+	 * create a deep copy of this Polynom
+	 * @return 
+	 */
+	@Override
+	public Polynom_able copy() {
+		Polynom p = new Polynom (this.toString());
+		return p;
+	}
+	/**
+	 * Compute a new Polynom which is the derivative of this Polynom
+	 * @return
+	 */
+	@Override
+	public Polynom_able derivative() {
+		Polynom_able p = new Polynom();
+		for (Monom monom : monoms) {
+			p.add(monom.derivative());
+		}
+		return p;
+	}
+	/**
+	 * Compute an integral from x0 to x1. 
+	 * @param x0 starting pooint
+	 * @param x1 end point
+	 * @return the approximated area above X-axis below this function bounded in the range of [x0,x1]
+	 */
+	@Override
+	public double area(double x0, double x1, double eps) {
+		Polynom_able p = new Polynom();
+		for (Monom monom : monoms) {
+			Monom m = new Monom(monom.get_coefficient()/(monom.get_power()+1), monom.get_power()+1);
+			p.add(m);
+		}
+		return p.f(x1) - p.f(x0);
+	}
+	/**
+	 * @return an Iterator (of Monoms) over this Polynom
+	 * @return
+	 */
+	@Override
+	public Iterator<Monom> iteretor() {
+		return monoms.iterator();
+	}
+	/**
+	 * Multiply this Polynom by Monom m1
+	 * @param m1
+	 */
+	@Override
+	public void multiply(Monom m1) {
+		for (Monom monom : monoms) {
+			monom.multipy(m1);
+		}
+	}
+	/**
+	 * get a String of the polynom
+	 * @return String
+	 */
+	@Override
+	public String toString() {
+		String out = "";
+		for (Monom m : monoms) {
+			out = out + m.toString();
+		}
+		return out;
+	}
+	
+	/**
+	 * compare powers
+	 * @author Adi
+	 *
+	 */
+	class Sortbypower implements Comparator<Monom> 
+	{ 
+	    // Used for sorting in ascending order of 
+	    // roll name 
+	    public int compare(Monom a, Monom b) 
+	    { 
+	        return Monom.comperator(a, b); 
+	    } 
+	}
+
+	
+	
+	@Override
+	public function initFromString(String s) {
+		return new Polynom(s);
+	} 
+	
+	
+	
+}
